310059b1c4a722a32c0eab7ef3b7e95f2bdb7ddd
Axel Faber <axelfaber@gmail.com>
2026-07-17 17:20:57 -0300

    Bajar Nonisec 100px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ export default function LandingPage() {
           </div>
 
           {/* Logos a la derecha - Nonisec debajo del lomo de DISTRIBUIDORA MAYORISTA */}
-          <div className="flex flex-col items-center justify-start gap-0" style={{ marginTop: '50px' }}>
+          <div className="flex flex-col items-center justify-start gap-0" style={{ marginTop: '100px' }}>
             {/* Logo Nonisec - Turquesa real, sin fondo */}
             <div style={{ lineHeight: 0, width: '100%', maxWidth: '500px' }}>
               <Image
